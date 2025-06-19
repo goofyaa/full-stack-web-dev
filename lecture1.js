1869ed100f225d07015ac3a858b0d5183d958481
niharika singh <nonisingh991@gmail.com>
2025-06-19 22:01:10 +0530

    this is my webdeb repo

---
+++ lecture1.js
@@ -0,0 +1,31 @@
+console.log("WELCOME TO APNA COLLEGE");
+fullname = "jija chad";
+console.log(fullname);
+price = 99.69;
+t = null;
+p = undefined;
+isfollow = false;
+iselite = true;
+let anida = "your mama";
+anida = "milf";
+let a;
+let x = BigInt("345");
+const student = {fullname : "niharika singh",
+                 age : 17,
+                 cgpa : 9.9,
+};
+
+student.age = student.age+1;
+            //OR
+student['age']= student['age']+1;
+
+//a variable to give data of instagram page
+const profile = {
+    username :"@naohfuri",
+    isFollow:false,
+    followers:920,
+    following:300,
+};
+
+console.log(typeof profile.username);
+//can even print the data typ inside the object
